764a26cba3b030e9b996ca8186e4a37642d75275
Rendani Gangazhe <rendanig@gmail.com>
2022-03-01 15:10:29 +0100

    Add reflector

--- renstrap-system.sh
+++ renstrap-system.sh
@@ -29,7 +29,7 @@ echo "[multilib]" >> $PACMAN_CONFIG
 echo "Include = /etc/pacman.d/mirrorlist" >> $PACMAN_CONFIG
 
 # Download base packages
-pacman -Syy --noconfirm networkmanager neovim vim base-devel intel-ucode xdg-utils xdg-user-dirs
+pacman -Syy --noconfirm networkmanager neovim vim base-devel intel-ucode xdg-utils xdg-user-dirs reflector
 
 # Boot boot loader
 mount -t efivarfs efivarfs /sys/firmware/efi/efivars/
@@ -50,6 +50,7 @@ echo " " && lsblk && echo " "
 read -p "What partition is your root mounted to (enter sdX)? " ROOT_PARTITION_NAME
 echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/$ROOT_PARTITION_NAME) rw" >> $BOOT_LOADER_CONFIG_PATH
 
+clear
 echo " " && echo "Please set a root password:"
 passwd
 
@@ -68,10 +69,12 @@ echo "Defaults rootpw" >> $SODOERS_FILE
 # Enable system services
 systemctl enable NetworkManager
 systemctl enable fstrim.timer
+systemctl enable reflector.timer
 
 # Update everything
 pacman -Syu --noconfirm
 
-print "Arch base setup done!"
+clear
+print "Arch base setup done!\n"
 
 exit 1;
